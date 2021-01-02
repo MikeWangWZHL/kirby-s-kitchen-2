8e5a74cbba511bd5c83ccdf142678194b7ee3525
mikewang <mikewangwzhl@gmail.com>
2021-01-02 11:36:33 -0600

    Initial Commit

---
+++ kirby's kitchen 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kirby's kitchen 2
+//
+//  Created by zhenhailong wang on 1/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
